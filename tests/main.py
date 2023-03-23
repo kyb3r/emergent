ccc03c01312027e9bf2ab64ed85342db141552be
kyb3r <abdurraqeeb53@gmail.com>
2023-03-24 08:19:24 +1100

    Use gpt-3.5 turbo

--- tests/main.py
+++ tests/main.py
@@ -1,21 +1,21 @@
 import logging
 from emergent.agent import ChatAgent, HierarchicalMemory
 from openai.embeddings_utils import get_embedding
-import config
 import openai
 
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
-openai.api_key = config.OPENAI_API_KEY # add your api key here
-memory_path = "emergent-main/memories.json"
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
+)
+memory_path = "memories.json"
 
 try:
     memory = HierarchicalMemory.from_json(memory_path)
     logging.info(f"Loaded memories from {memory_path}")
 except:
     logging.info(f"No {memory_path} file found, initializing new database")
-    memory = HierarchicalMemory()
+    memory = HierarchicalMemory(model="gpt-3.5-turbo")
 
-agent = ChatAgent(memory=memory, language_model="gpt-4")
+agent = ChatAgent(memory=memory, model="gpt-3.5-turbo")
 agent.memory.logs = []
 
 name = input("Please enter your name: ")
